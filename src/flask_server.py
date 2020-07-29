2eaa6d56cc90bb7ef90af8d6a454e3c5a3e9a900
Jalex-Yestera <yestera@gmail.com>
2020-07-29 16:25:48 +0200

    Server y Api a medias
    Signed-off-by: Jalex-Yestera <yestera@gmail.com>

--- src/flask_server.py
+++ src/flask_server.py
@@ -1,26 +1,39 @@
-# contains the functionality that starts the Flask Api @Jalex-Yestera
-'''There are two GET functions:
-    i. One that must allow to receive an group_id and will return a json with
-    one attribute called “token” with the S value (explained below). This
-    function must only return the S value in a json if the group_id received
-    is equal to N (explained below). Otherwise, it must return a string
-    with a message of error.
-
-    ii. Another that must allow you to receive a token_id value and, if
-    token_id is equal to S , return the json that contains the information of
-    the dataframe ( df ). Otherwise, return a string with a message of error.
-
-    1. N is the letter of your group concatenated with the sum of the
-    ages of the participants. Examples: “A103”, “C86”.
-
-    2. S is the letter of your group concatenated with the
-    multiplication of the DNIs of the participants of the group.
-    Example: “B85918591859851”, “D635154795182”'''
-
 import os, sys
 from flask import Flask, render_template, redirect, request, jsonify 
 import json
-import pandas
+
+html = """<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Página de prueba</title>
+</head>
+<body>
+<div id="main" class="full-width">
+    <h1>El título de la página</h1>
+    <p>Este es el primer párrafo</p>
+    <p>Este es el segundo párrafo</p>
+    <div id="innerDiv">
+        <div class="links">
+            <a href="https://pagina1.xyz/">Enlace 1</a>
+            <a href="https://pagina2.xyz/">Enlace 2</a>
+        </div>
+        <div class="right">
+            <div class="links">
+                <a href="https://pagina3.xyz/">Enlace 3</a>
+                <a href="https://pagina4.xyz/">Enlace 4</a>
+            </div>
+        </div>
+    </div>
+    <div id="footer">
+        <!-- El footer -->
+        <p>Este párrafo está en el footer</p>
+        <div class="links footer-links">
+            <a href="https://pagina5.xyz/">Enlace 5</a>
+        </div>
+    </div>
+</div>
+</body>
+</html>"""
 
 # ----------------------
 # $$$$$$$ FLASK $$$$$$$$
@@ -33,7 +46,7 @@ def default():
     # Redirect
     #return redirect("http://aiconscience.ddns.net", code=302)
     #return str(request.args)
-    return Print('Api working')
+    return html
 
 # ----------------------
 # $$$$$$$ FLASK GET $$$$$$$$
