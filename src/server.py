025a5f39f03b0e7e74519d82257aef63b047d0be
Jalex-Yestera <yestera@gmail.com>
2020-07-30 17:33:33 +0200

    Server.py con Api funcionando a falta de d_averages.json

--- src/server.py
+++ src/server.py
@@ -2,79 +2,39 @@ import os, sys
 from flask import Flask, render_template, redirect, request, jsonify 
 import json
 
-html = """<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <title>Página de prueba</title>
-</head>
-<body>
-<div id="main" class="full-width">
-    <h1>El título de la página</h1>
-    <p>Este es el primer párrafo</p>
-    <p>Este es el segundo párrafo</p>
-    <div id="innerDiv">
-        <div class="links">
-            <a href="https://pagina1.xyz/">Enlace 1</a>
-            <a href="https://pagina2.xyz/">Enlace 2</a>
-        </div>
-        <div class="right">
-            <div class="links">
-                <a href="https://pagina3.xyz/">Enlace 3</a>
-                <a href="https://pagina4.xyz/">Enlace 4</a>
-            </div>
-        </div>
-    </div>
-    <div id="footer">
-        <!-- El footer -->
-        <p>Este párrafo está en el footer</p>
-        <div class="links footer-links">
-            <a href="https://pagina5.xyz/">Enlace 5</a>
-        </div>
-    </div>
-</div>
-</body>
-</html>"""
-
 # ----------------------
 # $$$$$$$ FLASK $$$$$$$$
 # ----------------------
-
 app = Flask(__name__)  # init
-
 @app.route("/")  # Default path
 def default():
     # Redirect
-    #return redirect("http://aiconscience.ddns.net", code=302)
-    #return str(request.args)
-    return html
+    return '''<h1> Api del grupo B</h1> <p> Añadir get_token?id= para conseguir el token</p>'''
 
-# ----------------------
-# $$$$$$$ FLASK GET $$$$$$$$
-# ----------------------
-
-@app.route('/get', methods=['GET'])
-def get():
-    return '''<h1>Distant Reading Archive</h1>
-<p>A prototype API for distant reading of science fiction novels.</p>'''
-
-@app.route('/give_me_id', methods=['GET'])
-def give_id():
-    x = request.args['id']
-    return request.args
-
-# A route to return all of the available entries in our catalog.
-@app.route('/api/test/', methods=['GET'])
-def api_all():
-    restaurant_id = None
-    if 'id' in request.args:
-        restaurant_id = int(request.args['id'])
-    
-    if restaurant_id == 1:
-        return "{'json': true}"
-    elif restaurant_id == 99:
-        return jsonify(col_1=[3, 2, 1, 0], col_2=['a', 'b', 'c', 'd'])
+@app.route("/give_me_id", methods =['GET'])
+def give_id(): 
+    x= request.args['id']
+    return x
+@app.route("/get_token", methods = ['GET'])
+def get_token():
+    clave = None
+    yeison = { "token": "B23866390994081818446940" }
+    if 'id' in request.args: 
+        clave = str(request.args['id'])
+    if clave == 'B145':
+        return yeison
     else:
-        return "This is a message of error" + "<br>" + "<br>" + str(request.args)
+        return "Error al introducir la clave. Prueba con B145"
+@app.route("/get_json", methods = ['GET'])
+def get_json():
+    token = None
+    jason = d_averages.json
+    if 'id' in request.args:
+        token = str(request.args['id'])
+    if token == 'B23866390994081818446940':
+        return jason
+
+
 
 # ----------------------
 # $$$$$$$ MAIN $$$$$$$$
@@ -105,3 +65,8 @@ def main():
             
 if __name__ == "__main__":
     main()
+
+
+
+    # S del grupo es B23866390994081818446940
+    # N del grupo es B145
