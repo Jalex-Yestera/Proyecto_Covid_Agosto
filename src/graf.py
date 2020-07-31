ed78c5a8a56ac44eb18ded0a712d27dce1557754
CristiDatas <cristidatas@gmail.com>
2020-07-31 14:02:31 +0200

    modulo grafico A bueno

--- src/graf.py
+++ src/graf.py
@@ -1,12 +1,8 @@
-# Éste módulo contiene la función que hace el gráfico del json del equipo A @CristiDatas
-
 import pandas as pd
 from matplotlib import pyplot as plt
 
-def graf_a (graf):
+def graf_a(graf):
     pl=pd.read_json(graf)
     pl.plot()
     return plt.show()
-
-
 graf_a(graf="grupoa.json")
