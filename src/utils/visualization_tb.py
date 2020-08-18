ecd7c8ccc949581398fbfe14c3222c51152f7fb0
CristiDatas <cristidatas@gmail.com>
2020-08-18 17:56:00 +0200

    graf en visualization

--- src/utils/visualization_tb.py
+++ src/utils/visualization_tb.py
@@ -1,20 +1,9 @@
 # This module is dedicated to all plot functions
 
-# Éste módulo contiene la función que hace el gráfico del json del equipo A @CristiDatas
+# Este módulo contiene la función que hace el gráfico del json del equipo A. @CristiDatas:
 # graf
 def graf_a(graf):
     pl=pd.read_json(graf)
     pl.plot()
     return plt.show()
-graf_a(graf="grupoa.json")
-
-# Éste módulo contiene la función que hace el gráfico del json del equipo A @CristiDatas
-# graficar
-
-def graf_a (graf):
-    pl=pd.read_json(graf)
-    pl.plot()
-    return plt.show()
-
-
-graf_a(graf="grupoa")
+graf_a(graf="resources\json\output\grupoa.json")
